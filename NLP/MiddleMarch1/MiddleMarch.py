7ccbc53216069db92931df063c48b1dce9b7ba2c
Kevin Rocca <abc@example.com>
2023-02-06 20:56:11 -0800

    Add Code

--- NLP/MiddleMarch1/MiddleMarch.py
+++ NLP/MiddleMarch1/MiddleMarch.py
@@ -1,5 +1,10 @@
 import string
 import re
+import matplotlib.pyplot as plt
+import math
+#I am aware that this code is really hard to read and kinda weirdly written but this
+#is a practice program so I will not be fixing it.
+
 
 def main():
     #This clears the file so i dont keep adding stuff
@@ -45,8 +50,13 @@ def removePunc(str):
 
 def processToDict():
     tokensToDict = open('tokenized', 'r')
+    clearFile = open('finalcount', 'w')
+    clearFile.write("")
+    clearFile.close()
     processedFile = open('finalcount', 'a')
     wordDict = {}
+
+
     #This is the type of thing I was taught to do in java with scanners. Is there a better way?
     for line in tokensToDict:
         listOfWords = line.split()
@@ -56,20 +66,36 @@ def processToDict():
             else:
                 wordDict[word] = 1
 
-    #How do i display this pretty
-    print(wordDict)
+    #.items takes the key value pairs in a dictionary and turns them into a list of tuples.
+    #Each tuple has a key string and a value string. It then uses sorted to sort the dict.
+    #This would sort by key instead of value so I use the key= thing which is a part of sort
+    #This determines what it will be sorted by. I then add an anonymous function that
+    #takes in a tuple from the items thing and returns the tuple[1] which is the key
+    #This is still really hard for me to do quickly but I get the concept.
+    #Because I made it so complex I may as well reverse it so the most frequent word
+    #is put at the beginning.
+    sortedByVal =  dict(sorted(wordDict.items(), key=lambda v : v[1], reverse = True))
 
-    #.items takes the key value pairs in a dictionary and turns them into a list of tuples. Each
-    #Each tuple has a key string and a value string
+    #yucky list comprehension way. I think I need the key, value otherwise
+    #it grabs the tuple to i. Is there any way to specify this?
+    x = [math.log(i+1) for i, (key, value) in enumerate(sortedByVal.items())]
+    y = [math.log(value) for key, value in sortedByVal.items()]
+    graphZipf(x, y)
 
-    #How do i sort a dictionary?
-    #dict(sorted(wordDict.items()))
+    #normal human way
+    #for i, (key, value) in enumerate(sortedByVal.items()):
+    #    x.append(i)
+    #    y.append(value)
 
-    for key, value in wordDict.items():
+    for key, value in sortedByVal.items():
         processedFile.write('%s: %d\n' % (key, value))
     
     tokensToDict.close()
     processedFile.close()
 
+def graphZipf(x,y):
+    plt.plot(x, y) #takes lists!
+
+    plt.show()
 
 main()
